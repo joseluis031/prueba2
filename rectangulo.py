cf6980594abe1202c35da2bb0e1bbb5106af2bfd
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-09-21 17:57:54 +0200

    funciones del rectangulo

--- rectangulo.py
+++ rectangulo.py
@@ -23,3 +23,9 @@ class rectangulo:
         self.b = b
     def base(self):
         print("la base es {} ".format(self.x2 - self.x1))
+        
+    def altura(self):
+        print("la altura es {}".format(self.y2 - self.y1))
+        
+    def area(self):
+        print("El area es {}".format((self.x2-self.x1)*(self.y2-self.y1))
