fe6de52514bce9bac5bec550d4251b2cca19a66e
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-09-21 17:03:40 +0200

    funcion cuadrante

--- ejercicio.py
+++ ejercicio.py
@@ -1,7 +1,18 @@
 import math
-
+X = 0
+Y = 0
 
 class Punto:
     def __init__(self,X,Y):
         self.X = X
         self.Y = Y
+    def cuadrante(self):
+        if self.X > 0 and self.Y > 0:
+            print( "Pertenece al primer cuadrante")
+        elif self.X > 0 and self.Y < 0:
+            print( "Pertenece al 4º cuadrante")
+        elif self.X < 0 and self.Y > 0:
+            print( "Pertenece al 2º cuadrante")
+        elif self.X < 0 and self.Y > 0:
+            print( "Pertenece al 4º cuadrante")
+        
